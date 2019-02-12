67c65f5a7162ecaeff5877c94eed6281fc60e340
Sham <shambhavi.srivastava@toyotaconnected.com>
2019-02-11 20:59:56 -0600

    more fun

--- BST/Traversal.py
+++ BST/Traversal.py
@@ -15,3 +15,35 @@ order of traversal
 ]
 '''
 
+class Node:
+
+    def __init__(self, key):
+        self.data = key
+        self.left = None
+        self.right = None
+
+class solution:
+
+    def levelorder(self, root):
+
+        """
+        :param root: tree node
+        :return: list of list of integers
+        """
+        result = []
+        if not root: return result
+
+        cur_level = [root]
+        # loop until empty
+        while cur_level != []:
+            vals = []
+            new_level = []
+            for node in cur_level:
+                vals.append(node.data)
+                if node.left:
+                    new_level.append(node.left)
+                if node.right:
+                    new_level.append(node.right)
+            result.append(vals)
+            cur_level = new_level
+        return result
